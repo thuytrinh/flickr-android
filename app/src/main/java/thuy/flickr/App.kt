1d2c9f344e8abf04eec57e327793f5d25013f51b
Thuy Trinh <thuy.ntrinh@gmail.com>
2017-10-16 23:32:52 +0700

    Add Fresco

--- app/src/main/java/thuy/flickr/App.kt
+++ app/src/main/java/thuy/flickr/App.kt
@@ -1,9 +1,15 @@
 package thuy.flickr
 
+import com.facebook.drawee.backends.pipeline.Fresco
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
 class App : DaggerApplication() {
+  override fun onCreate() {
+    super.onCreate()
+    Fresco.initialize(this)
+  }
+
   override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
       DaggerAppComponent.builder().create(this)
 }
