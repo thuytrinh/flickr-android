41ed4eb79a5db3c709b5846fcc8a45738bb3b11c
Thuy Trinh <thuy.ntrinh@gmail.com>
2017-10-20 03:09:47 +0700

    Show process

--- app/src/main/java/thuy/flickr/photodetails/PhotoDetailsFragment.kt
+++ app/src/main/java/thuy/flickr/photodetails/PhotoDetailsFragment.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.facebook.drawee.drawable.ProgressBarDrawable
+import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder
 import dagger.android.support.DaggerFragment
 import thuy.flickr.databinding.PhotoDetailsBinding
 import javax.inject.Inject
+
 
 class PhotoDetailsFragment : DaggerFragment() {
   @Inject lateinit var viewModel: PhotoDetailsViewModel
@@ -27,6 +30,13 @@ class PhotoDetailsFragment : DaggerFragment() {
     binding.toolbar.setNavigationOnClickListener {
       activity.finish()
     }
+
+    // See http://frescolib.org/docs/using-simpledraweeview.html.
+    val hierarchy = GenericDraweeHierarchyBuilder.newInstance(resources)
+        .setProgressBarImage(ProgressBarDrawable())
+        .build()
+    binding.photoView.hierarchy = hierarchy
+
     binding.viewModel = viewModel
     return binding.root
   }
