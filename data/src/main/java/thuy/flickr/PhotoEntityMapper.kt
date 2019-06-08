e9a1e700f6f577c596dbf3c6968cb909c1cdb638
Thuy Trinh <thuy.trinh@cobi.bike>
2019-06-08 15:55:11 +0200

    Fix detekt reports on param naming

--- data/src/main/java/thuy/flickr/PhotoEntityMapper.kt
+++ data/src/main/java/thuy/flickr/PhotoEntityMapper.kt
@@ -8,37 +8,37 @@ internal open class PhotoEntityMapper {
       server(it.server)
       farm(it.farm)
       title(it.title)
-      url_l(it.url_l)
-      url_o(it.url_o)
-      url_c(it.url_c)
-      url_z(it.url_z)
-      url_n(it.url_n)
-      url_m(it.url_m)
-      url_q(it.url_q)
-      url_s(it.url_s)
-      url_t(it.url_t)
-      url_sq(it.url_sq)
+      url_l(it.urlL)
+      url_o(it.urlO)
+      url_c(it.urlC)
+      url_z(it.urlZ)
+      url_n(it.urlN)
+      url_m(it.urlM)
+      url_q(it.urlQ)
+      url_s(it.urlS)
+      url_t(it.urlT)
+      url_sq(it.urlSq)
     }.build()
   }
 
   open fun toDbEntities(entities: List<PhotoEntity>): List<PhotoDbEntity> =
-      entities.map {
-        PhotoDbEntity(
-            id = it.id(),
-            secret = it.secret(),
-            server = it.server(),
-            farm = it.farm(),
-            title = it.title(),
-            url_l = it.url_l(),
-            url_o = it.url_o(),
-            url_c = it.url_c(),
-            url_z = it.url_z(),
-            url_n = it.url_n(),
-            url_m = it.url_m(),
-            url_q = it.url_q(),
-            url_s = it.url_s(),
-            url_t = it.url_t(),
-            url_sq = it.url_sq()
-        )
-      }
+    entities.map {
+      PhotoDbEntity(
+        id = it.id(),
+        secret = it.secret(),
+        server = it.server(),
+        farm = it.farm(),
+        title = it.title(),
+        urlL = it.url_l(),
+        urlO = it.url_o(),
+        urlC = it.url_c(),
+        urlZ = it.url_z(),
+        urlN = it.url_n(),
+        urlM = it.url_m(),
+        urlQ = it.url_q(),
+        urlS = it.url_s(),
+        urlT = it.url_t(),
+        urlSq = it.url_sq()
+      )
+    }
 }
