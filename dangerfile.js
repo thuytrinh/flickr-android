c08ccf9616d6f2a10dcb50642fc74f5fad577620
Thuy Trinh <thuy.trinh@cobi.bike>
2019-06-12 16:38:10 +0200

    Create dangerfile.js

---
+++ dangerfile.js
@@ -0,0 +1,6 @@
+const { danger, warn } = require("danger");
+
+// No pull request is too small to include a description of why you made a change.
+if (danger.github.pr.body.length < 10) {
+  warn("Please include a description of your PR changes.");
+}
